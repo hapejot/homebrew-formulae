bf239315a89781f92e44e29205d689921594bbdb
hapejot <38452720+hapejot@users.noreply.github.com>
2020-08-28 09:52:32 +0200

    Update arm-none-eabi-gcc.rb

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -6,7 +6,7 @@ class ArmNoneEabiGcc < Formula
   version '9-2019-q4-major'
 
   url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2020q2/gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2'
-
+  sha256 '5adc2ee03904571c2de79d5cfc0f7fe2a5c5f54f44da5b645c17ee57b217f11f'
   def install
     (prefix/"gcc").install Dir["./*"]
     Dir.glob(prefix/"gcc/bin/*") { |file| bin.install_symlink file }
